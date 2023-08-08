b76a89ff599e47fef3927eb1fb392351be2c4f80
Dr. Sasha Göbbels <sasha.goebbels@overleaf.com>
2023-08-08 16:06:36 +0200

    node based sprite generator added

---
+++ generate.js
@@ -0,0 +1,8 @@
+var svgstore = require("svgstore");
+var fs = require("fs");
+
+var sprites = svgstore()
+  .add("unicorn", fs.readFileSync("./assets/unicorn.svg", "utf8"))
+  .add("rainbow", fs.readFileSync("./assets/rainbow.svg", "utf8"));
+
+fs.writeFileSync("./sprites.svg", sprites);
